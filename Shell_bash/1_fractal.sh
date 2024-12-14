b100c157ac79cdbc95c03452ce275d1cd4aa28bd
RomaF41 <106123923+rmoncada21@users.noreply.github.com>
2024-12-13 23:41:45 -0600

    update 1_fractal.sh with _draw_vertical_ones function

--- Shell_bash/1_fractal.sh
+++ Shell_bash/1_fractal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# N=5
+# filas=$(((2**N) - 1))
+# echo "filas=$filas"
 filas=63
 columnas=100
 
@@ -18,9 +20,9 @@ function llenar_arreglo(){
 }
 
 # Imprimir "arreglo"
-echo "Imprimir arreglo"
-llenar_arreglo arreglo
-echo "${arreglo[*]}"
+# echo "Imprimir arreglo"
+# llenar_arreglo arreglo
+# echo "${arreglo[*]}"
 
 # Función para llenar el arreglo asociativo
 function llenar_arreglo_asociativo(){
@@ -42,10 +44,10 @@ function print_arreglo_asociativo(){
 }
 
 # Imprimir "arreglo_asociativo"
-echo "Sección de arreglos asociativos"
-llenar_arreglo_asociativo asociativo arreglo
-echo
-print_arreglo_asociativo asociativo
+# echo "Sección de arreglos asociativos"
+# llenar_arreglo_asociativo asociativo arreglo
+# echo
+# print_arreglo_asociativo asociativo
 
 
 # Función para quitar los espacios en blanco dentro de los arreglos
@@ -55,15 +57,77 @@ function remove_whitespace(){
     for item in "${!local_arreglo_aso[@]}"; do
         # acceder al arreglo temporal
         nueva_linea=$(echo "${local_arreglo_aso[$item]}" | sed 's/ //g')
-        # echo "$nueva_linea"
+        echo "$nueva_linea"
     done
 }
 
-remove_whitespace asociativo
-print_arreglo_asociativo asociatio
+# remove_whitespace asociativo
+# print_arreglo_asociativo asociatio
 
 # TODO: implement function logic to make a tree pattern
 function tree_pattern(){
     local -n local_arreglo_aso="$1"
 }
 
+function _count_tree(){
+    local -n local_filas="$1"
+    contador=2;
+
+    while (( contador < local_filas )); do
+        # echo "$contador"
+        arreglo_contador+=($contador)
+        contador=$(echo "$contador*2" | bc)
+    done
+
+    # echo " contador = $contador"
+}
+
+# _count_tree filas
+# echo "${arreglo_contador[@]}"
+
+
+function _draw_vertical_ones(){
+    local -n local_arreglo_aso1="$1"
+    local -n local_arreglo_count="$2"
+
+    # for para hacer/ver la cantidad de árboles
+    for (( j="${#local_arreglo_count[@]} - 1"; j>=0; j--)); do
+        # echo "$j"
+        altura_arbol=${local_arreglo_count[$j]}
+        suma_arbol=$(($suma_arbol + ${local_arreglo_count[$j]}))
+        
+        nuevo_limite=$suma_arbol
+
+        echo $suma_arbol
+
+        # for para recorrer el arreglo asociativo
+        for (( k="$filas-1"; k>=0; k-- )); do
+            local_arreglo_temp1=(${local_arreglo_aso1[$k]})
+            # local_arreglo_temp1[49]="1"
+            # local_arreglo_aso1[$k]="${local_arreglo_temp1[@]}"
+
+            if (( k >= ( filas - suma_arbol ) )); then
+                local_arreglo_temp1[49]="1"
+                local_arreglo_aso1[$k]="${local_arreglo_temp1[@]}"
+                # echo -n
+            fi
+
+            # nueva_altura=(($filas))
+        done
+
+    done
+}
+
+function _draw_horizontal_ones(){
+    echo
+}
+
+llenar_arreglo arreglo
+llenar_arreglo_asociativo asociativo arreglo
+_count_tree filas
+# echo "${arreglo_contador[@]}"
+
+_draw_vertical_ones asociativo arreglo_contador
+# echo "${asociativo[63]}"
+# print_arreglo_asociativo asociativo
+remove_whitespace asociativo
